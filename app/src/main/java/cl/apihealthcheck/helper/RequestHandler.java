6d965a05bca9fe852acf406dfbe18ae5be477a10
Ignacio Thomson <ignacio.thomson.97@gmail.com>
2026-02-17 22:53:18 -0300

    Cambio en tipo de dato devuelto

--- app/src/main/java/cl/apihealthcheck/helper/RequestHandler.java
+++ app/src/main/java/cl/apihealthcheck/helper/RequestHandler.java
@@ -5,7 +5,6 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.util.logging.Logger;
 
@@ -13,14 +12,14 @@ public class RequestHandler {
 
     private static final Logger LOGGER = Logger.getLogger(RequestHandler.class.getName());
 
-    public static HttpResponse<String> buildRequest(String uri) {
+    public static int buildRequest(String uri) {
         try {
             HttpClient httpClient = HttpClient.newHttpClient();
             HttpRequest request = HttpRequest.newBuilder().uri(new URI(uri)).GET().build();
-            return httpClient.send(request, BodyHandlers.ofString());
+            return httpClient.send(request, BodyHandlers.ofString()).statusCode();
         } catch (IOException | InterruptedException | URISyntaxException e) {
             LOGGER.warning("Ha ocurrido una excepción al solicitar el servicio.");
-            return null;
+            return 0;
         }
     }
 }
