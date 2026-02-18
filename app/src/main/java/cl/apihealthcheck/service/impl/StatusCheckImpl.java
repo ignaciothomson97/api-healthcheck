4d6e30139d13ff65e1d38e48e56d80f2b87c7cd7
Ignacio Thomson <ignacio.thomson.97@gmail.com>
2026-02-17 22:54:59 -0300

    Cambios de lógica de servicio

--- app/src/main/java/cl/apihealthcheck/service/impl/StatusCheckImpl.java
+++ app/src/main/java/cl/apihealthcheck/service/impl/StatusCheckImpl.java
@@ -4,31 +4,55 @@ import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import cl.apihealthcheck.constants.Constants;
+import cl.apihealthcheck.entity.ApiRequest;
 import cl.apihealthcheck.helper.RequestHandler;
+import cl.apihealthcheck.repository.ApiRequestRepository;
 import cl.apihealthcheck.service.Healthcheck;
 
 public class StatusCheckImpl implements Healthcheck {
 
     private final ExecutorService ioExecutor = Executors.newFixedThreadPool(4);
+    private final static Logger LOGGER = Logger.getLogger(StatusCheckImpl.class.getName());
 
     @Override
     public void parallelCheck() {
-        List<String> targets = List.of(
-            Constants.REDDIT_API,
-            Constants.LINKEDIN_API, 
-            Constants.DISCORD_API, 
-            Constants.GITHUB_API);
-
+        List<String> targets = buildTargets();
         for (String target : targets) {
             CompletableFuture.supplyAsync(() -> RequestHandler.buildRequest(target), ioExecutor)
-                .thenAccept(result -> System.err.println("Resultado " + result + " - Status: " + result.statusCode()))
+                .thenAccept(result -> {
+                    persistData(buildApiRequest(target, result));
+                })
                 .exceptionally(ex -> {
-                    System.out.println("Error");
+                    LOGGER.severe("Error en operación asíncrona");
                     return null;
                 });
         }
     }
 
+    private List<String> buildTargets() {
+        return List.of(
+            Constants.REDDIT_API,
+            Constants.LINKEDIN_API, 
+            Constants.DISCORD_API, 
+            Constants.GITHUB_API
+        );
+    }
+
+    private ApiRequest buildApiRequest(String target, int result) {
+        return new ApiRequest.Builder()
+            .apiName(target)
+            .lastStatus(result)
+            .isUp(result <= 300 && result >= 200)
+            .lastErrorMessage("")
+            .build();
+    }
+
+    private void persistData(ApiRequest apiRequest) {
+        LOGGER.log(Level.INFO, "Persistiendo resultado de: {0} (Status: {1})", new Object[]{apiRequest.getApiName(), apiRequest.getLastStatus()});
+        ApiRequestRepository.upsertStatus(apiRequest);
+    }
 }
