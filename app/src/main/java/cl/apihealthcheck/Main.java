589de407d6b337f3cdf49d3c8beb47ca80247b7c
Ignacio Thomson <ignacio.thomson.97@gmail.com>
2026-02-17 22:53:06 -0300

    Config logging de sistema

--- app/src/main/java/cl/apihealthcheck/Main.java
+++ app/src/main/java/cl/apihealthcheck/Main.java
@@ -3,6 +3,7 @@ package cl.apihealthcheck;
 import cl.apihealthcheck.helper.SchedulerExecutor;
 
 public class Main {
+    static { System.setProperty("java.util.logging.SimpleFormatter.format", "[%4$s] %5$s %n"); }
     public static void main(String[] args) {
        SchedulerExecutor.start();
     }
