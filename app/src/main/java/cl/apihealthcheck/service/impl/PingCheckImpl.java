ab60eab4cb946964131799ba65077a9b1a8e1b4c
Ignacio Thomson <ignacio.thomson.97@gmail.com>
2026-02-21 21:03:25 -0300

    Removed unnecessary service implementation.

--- app/src/main/java/cl/apihealthcheck/service/impl/PingCheckImpl.java
+++
@@ -1,9 +0,0 @@
-package cl.apihealthcheck.service.impl;
-
-import cl.apihealthcheck.service.Healthcheck;
-
-public class PingCheckImpl implements Healthcheck {
-    @Override
-    public void parallelCheck() {
-    }
-}
