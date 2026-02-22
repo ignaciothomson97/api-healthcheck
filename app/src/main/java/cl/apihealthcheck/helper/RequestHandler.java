ad7224ca849fcb5085376d01aea48e9b4686b92a
Ignacio Thomson <ignacio.thomson.97@gmail.com>
2026-02-21 21:02:51 -0300

    Changes needed to bypass firewall, implement retry and timeout pattern, and improve error logging. Also changed return of int for record RestResponse.

--- app/src/main/java/cl/apihealthcheck/helper/RequestHandler.java
+++ app/src/main/java/cl/apihealthcheck/helper/RequestHandler.java
@@ -1,25 +1,69 @@
 package cl.apihealthcheck.helper;
 
+import cl.apihealthcheck.model.RestResponse;
+
 import java.io.IOException;
+import java.net.ConnectException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.net.http.HttpTimeoutException;
+import java.time.Duration;
 import java.util.logging.Logger;
 
 public class RequestHandler {
 
     private static final Logger LOGGER = Logger.getLogger(RequestHandler.class.getName());
+    private static final HttpClient HTTP_CLIENT = HttpClient.newBuilder()
+            .connectTimeout(Duration.ofSeconds(5)).build();
 
-    public static int buildRequest(String uri) {
+    public static RestResponse buildRequest(String uri) {
+        int maxRetries = 3;
+        int attempt = 0;
+        while (true) {
+            try {
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(new URI(uri))
+                        .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " +
+                                "(KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
+                        .header("Accept", "application/json")
+                        .timeout(Duration.ofSeconds(15)) // Patrón 'Timeout'
+                        .GET()
+                        .build();
+                HttpResponse<String> response = HTTP_CLIENT.send(request, BodyHandlers.ofString());
+                String errorMessage = (response.statusCode() >= 400) ? response.body() : "";
+
+                return new RestResponse(response.statusCode(), response.body(), errorMessage);
+            } catch (ConnectException e) {
+                // Patrón 'Retry'
+                final int currentAttempt = attempt++;
+                LOGGER.warning(() -> "Intento " + currentAttempt + " fallido para " + uri + " (Conexión rechazada). Reintentando...");
+
+                if (attempt < maxRetries) {
+                    waitUntilRetry();
+                } else {
+                    LOGGER.severe("Se agotaron los reintentos para " + uri);
+                    return new RestResponse(0, "", "ConnectException tras " + maxRetries + " intentos");
+                }
+            } catch (HttpTimeoutException e) {
+                return new RestResponse(408, "", "Timeout - La API ha tardado demasiado " +
+                        "en responder");
+            } catch (IOException | InterruptedException | URISyntaxException e) {
+                LOGGER.severe(() -> "Ha ocurrido una excepción al solicitar el servicio: " + uri + " - " +
+                        e.getClass().getSimpleName() + " - Mensaje: " + e.getMessage());
+                return new RestResponse(0, "", e.getMessage());
+            }
+        }
+    }
+
+    private static void waitUntilRetry() {
         try {
-            HttpClient httpClient = HttpClient.newHttpClient();
-            HttpRequest request = HttpRequest.newBuilder().uri(new URI(uri)).GET().build();
-            return httpClient.send(request, BodyHandlers.ofString()).statusCode();
-        } catch (IOException | InterruptedException | URISyntaxException e) {
-            LOGGER.severe(() -> "Ha ocurrido una excepción al solicitar el servicio: " + uri + " - " + e.getMessage());
-            return 0;
+            Thread.sleep(2000);
+        } catch (InterruptedException ex) {
+            Thread.currentThread().interrupt();
         }
     }
 }
