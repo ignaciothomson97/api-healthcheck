09fed786ba7bade4be00cf95a3577c0f4b78cf47
Ignacio Thomson <ignacio.thomson.97@gmail.com>
2026-02-23 21:10:48 -0300

    Changed Scheduler logic to accept a dynamic list of jobs to execute.

--- app/src/main/java/cl/apihealthcheck/helper/SchedulerExecutor.java
+++ app/src/main/java/cl/apihealthcheck/helper/SchedulerExecutor.java
@@ -1,41 +1,27 @@
 package cl.apihealthcheck.helper;
 
+import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
+import cl.apihealthcheck.records.ScheduledJob;
+import cl.apihealthcheck.repository.RequestRepository;
 import cl.apihealthcheck.service.StatusCheckService;
 import cl.apihealthcheck.service.impl.StatusCheckServiceImpl;
 
 public class SchedulerExecutor {
     private static final Logger LOGGER = Logger.getLogger(SchedulerExecutor.class.getName());
     private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-
     private final StatusCheckService statusCheck = new StatusCheckServiceImpl();
-    private final CsvExport csvExport = new CsvExport();
+    private final CsvExport csvExport = new CsvExport(new RequestRepository());
+    private final List<ScheduledJob> scheduledJobs = buildScheduledJobs();
 
-    /* Estoy pensando dejar este metodo aún más generico, para pasar como argumento la funcionalidad y no tener que
-      añadir un nuevo scheduleWithFixedDelay por cada funcionalidad asinconra de la APP.
-      Por el momento, se queda así */
     public void start() {
         LOGGER.info("Iniciando Monitor de APIs...");
-
-        scheduler.scheduleWithFixedDelay(() -> {
-            try {
-                statusCheck.parallelCheck();
-            } catch (RuntimeException ex) {
-                LOGGER.severe(() -> "Ha ocurrido una excepción al consultar la API - " + ex.getMessage());
-            }
-        }, 0, 60, TimeUnit.SECONDS);
-
-        scheduler.scheduleWithFixedDelay(() -> {
-            try {
-                csvExport.exportAndCleanRecords();
-            } catch (RuntimeException ex) {
-                LOGGER.severe(() -> "Ha ocurrido una excepción al intentar exportar y limpiar la BD - " + ex.getMessage());
-            }
-        }, 0, 60, TimeUnit.SECONDS);
+        if (scheduledJobs.isEmpty()) LOGGER.warning("No hay trabajos programados");
+        for (ScheduledJob scheduledJob : scheduledJobs) registerJob(scheduledJob);
     }
 
     public void stopAll() {
@@ -51,5 +37,32 @@ public class SchedulerExecutor {
             Thread.currentThread().interrupt();
             LOGGER.severe(() -> "Ha ocurrido una excepción al intentar detener el Scheduler - " + ex.getMessage());
         }
+    }
+
+    private void registerJob(ScheduledJob scheduledJob) {
+        scheduler.scheduleWithFixedDelay(() -> {
+           try {
+               scheduledJob.action().run();
+           } catch (Throwable ex) {
+               LOGGER.severe(() -> "Ha ocurrido una excepción al ejecutar el trabajo '" + scheduledJob.name() + "' - " + ex.getMessage());
+           }
+        }, scheduledJob.initialDelay(), scheduledJob.period(), scheduledJob.timeUnit());
+        LOGGER.info(() -> String.format("Trabajo '%s' programado con periodo de %d %s",
+                scheduledJob.name(), scheduledJob.period(), scheduledJob.timeUnit()));
+    }
+
+    private List<ScheduledJob> buildScheduledJobs() {
+        return List.of(
+                new ScheduledJob(
+                        "StatusCheck",
+                        statusCheck::parallelCheck,
+                        0, 60, TimeUnit.SECONDS
+                ),
+                new ScheduledJob(
+                        "CsvExport",
+                        csvExport::exportRecords,
+                        0, 60, TimeUnit.MINUTES
+                )
+        );
     }
 }
